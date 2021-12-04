c83dd3ee768b497d50dbd2639ea9cfa5fdacf92f
majerzemilia <emilia.majerz@comarch.com>
2021-12-04 01:46:30 +0100

    Remove unused library

--- day3b.py
+++ day3b.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 
 numbers = []
@@ -11,7 +10,7 @@ number_len = len(numbers[0])
 
 oxygen_generator_rating = numbers[:]
 
-i=0
+i = 0
 while(len(oxygen_generator_rating) > 1 and i < number_len):
     current_digits = [int(entry[i]) for entry in oxygen_generator_rating]
     current_digit = '1' if sum(current_digits) >= 0.5 * len(current_digits) else '0'
@@ -20,7 +19,7 @@ while(len(oxygen_generator_rating) > 1 and i < number_len):
 
 CO2_scrubber_rating = numbers[:]
 
-i=0
+i = 0
 while(len(CO2_scrubber_rating) > 1 and i < number_len):
     current_digits = [int(entry[i]) for entry in CO2_scrubber_rating]
     current_digit = '1' if sum(current_digits) < 0.5 * len(current_digits) else '0'
