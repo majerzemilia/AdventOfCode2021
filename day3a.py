9c19f9dfff3dce65610272ae3242264e9837943b
majerzemilia <emilia.majerz@comarch.com>
2021-12-04 01:39:38 +0100

    Add day3 solutions

---
+++ day3a.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+
+numbers = []
+
+with open(os.path.join("data", "day3.txt"), mode="r") as f:
+    for line in f:
+        numbers.append(line.strip())
+
+number_len = len(numbers[0])
+
+gamma_rate = []
+
+for i in range(number_len):
+    current_digits = [int(entry[i]) for entry in numbers]
+    gamma_rate.append('1' if sum(current_digits) >= 0.5 * len(current_digits) else '0')
+
+epsilon_rate = ['0' if gamma == '1' else '1' for gamma in gamma_rate]
+
+gamma_rate = int("".join(gamma_rate), 2)
+epsilon_rate = int("".join(epsilon_rate), 2)
+
+print(gamma_rate * epsilon_rate)
---
+++ day3b.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+
+numbers = []
+
+with open(os.path.join("data", "day3.txt"), mode="r") as f:
+    for line in f:
+        numbers.append(line.strip())
+
+number_len = len(numbers[0])
+
+oxygen_generator_rating = numbers[:]
+
+i=0
+while(len(oxygen_generator_rating) > 1 and i < number_len):
+    current_digits = [int(entry[i]) for entry in oxygen_generator_rating]
+    current_digit = '1' if sum(current_digits) >= 0.5 * len(current_digits) else '0'
+    oxygen_generator_rating = list(filter(lambda x: x[i] == current_digit, oxygen_generator_rating))
+    i += 1
+
+CO2_scrubber_rating = numbers[:]
+
+i=0
+while(len(CO2_scrubber_rating) > 1 and i < number_len):
+    current_digits = [int(entry[i]) for entry in CO2_scrubber_rating]
+    current_digit = '1' if sum(current_digits) < 0.5 * len(current_digits) else '0'
+    CO2_scrubber_rating = list(filter(lambda x: x[i] == current_digit, CO2_scrubber_rating))
+    i += 1
+
+oxygen_generator_rating = int("".join(oxygen_generator_rating), 2)
+CO2_scrubber_rating = int("".join(CO2_scrubber_rating), 2)
+
+print(oxygen_generator_rating * CO2_scrubber_rating)
