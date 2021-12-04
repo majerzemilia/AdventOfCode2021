4554b005a7694d26da705176e93810608e0c0b83
majerzemilia <majerzemilia@gmail.com>
2021-12-04 18:43:53 +0100

    Simplify data loading

--- day4b.py
+++ day4b.py
@@ -16,25 +16,12 @@ def check_cols_sum(masks):
     prod = arr @ vec
     return np.where(prod == width)[0] // width
 
-boards = []
-curr_arr = None
 
 with open(os.path.join("data", "day4.txt"), mode="r") as f:
-    for i, line in enumerate(f):
-        line = line.strip()
-        if i == 0:
-            input_numbers = [int(num) for num in line.split(',')]
-            continue
-        if line == '':
-            if curr_arr is not None:
-                boards.append(curr_arr)
-            curr_arr = []
-            continue
-        curr_input = [int(number) for number in line.split()]
-        curr_arr.append(curr_input)
-    boards.append(curr_arr)
-    
-boards = np.array(boards)
+    input_numbers = [int(num) for num in next(f).strip().split(',')]
+    boards = np.loadtxt(f)
+    boards = boards.reshape((-1, boards.shape[1], boards.shape[1]))
+
 masks = np.zeros(boards.shape)
 
 finished_arrays = np.array([False] * len(boards))
