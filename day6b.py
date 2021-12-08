beb1b8340e6ec8a73952c6df17a907c3279c5c15
majerzemilia <majerzemilia@gmail.com>
2021-12-08 12:08:40 +0100

    Small code refactoring

--- day6b.py
+++ day6b.py
@@ -3,8 +3,7 @@ import os
 
 def how_many_fish(lantern_fish_cnts, days_to_check):
     while days_to_check > 0:
-        lantern_fish_cnts = {k: lantern_fish_cnts[k+1] for k in lantern_fish_cnts if k != 9}
-        lantern_fish_cnts.update({9: 0})
+        lantern_fish_cnts = {k: (lantern_fish_cnts[k+1] if k != 9 else 0) for k in lantern_fish_cnts}
         lantern_fish_cnts[8] += lantern_fish_cnts[-1]
         lantern_fish_cnts[6] += lantern_fish_cnts[-1]
         lantern_fish_cnts[-1] = 0
@@ -12,8 +11,7 @@ def how_many_fish(lantern_fish_cnts, days_to_check):
     return sum(lantern_fish_cnts.values())
 
 
-lantern_fish_cnts = {k: 0 for k in range(-1, 9)}
-lantern_fish_cnts[9] = 0
+lantern_fish_cnts = {k: 0 for k in range(-1, 10)}
 
 with open(os.path.join("data", "day6.txt")) as f:
     for num in f.read().split(','):
